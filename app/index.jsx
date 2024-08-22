2d41a91ac6fcf11c4cab23992e2033512d157490
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-08-22 19:27:11 +0300

    adding navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
  //Homepage
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Aora!!</Text>
       <StatusBar style="auto" />
+      <Link href='/profile' style={{ color: 'blue '}}>Go to Profile</Link>
     </View>
   );
 }
